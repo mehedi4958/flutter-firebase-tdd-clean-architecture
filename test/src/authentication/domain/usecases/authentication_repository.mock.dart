102828922b3ca374284bf632cdc6a8aaa535a54a
Mehedi <mehedi4958@gmail.com>
2024-01-07 00:48:49 +0600

    test cases created for createUser and getUsers

---
+++ test/src/authentication/domain/usecases/authentication_repository.mock.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_firebase_tdd_clean_architecture/src/authentication/domain/repositories/authentication_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthenticationRepository extends Mock
+    implements AuthenticationRepository {}
--- test/src/authentication/domain/usecases/get_users_test.dart
+++ test/src/authentication/domain/usecases/get_users_test.dart
@@ -6,48 +6,40 @@
 // Answer -> Using the Mocktail's APIs
 
 import 'package:dartz/dartz.dart';
+import 'package:flutter_firebase_tdd_clean_architecture/src/authentication/domain/entities/user.dart';
 import 'package:flutter_firebase_tdd_clean_architecture/src/authentication/domain/repositories/authentication_repository.dart';
-import 'package:flutter_firebase_tdd_clean_architecture/src/authentication/domain/usecases/create_user.dart';
+import 'package:flutter_firebase_tdd_clean_architecture/src/authentication/domain/usecases/get_users.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 import 'authentication_repository.mock.dart';
 
 void main() {
-  late CreateUser usecase;
+  late GetUsers usecase;
   late AuthenticationRepository repository;
 
   setUp(() {
     repository = MockAuthenticationRepository();
-    usecase = CreateUser(repository);
+    usecase = GetUsers(repository);
   });
 
-  const params = CreateUserParams.empty();
+  const tResponse = [User.empty()];
 
   test(
-    'should call [AuthenticationRepository.createUser]',
+    'should call [AuthenticationRepository.getUsers] and return [List<User>]',
     () async {
       /// Arrange
       // STUB
-      when(
-        () => repository.createUser(
-          createdAt: any(named: 'createdAt'),
-          name: any(named: 'name'),
-          avatar: any(named: 'avatar'),
-        ),
-      ).thenAnswer((_) async => const Right(null));
+      when(() => repository.getUsers())
+          .thenAnswer((_) async => const Right(tResponse));
 
       /// Act
-      final result = await usecase(params);
+      final result = await usecase();
 
       /// Assert
-      expect(result, equals(const Right<dynamic, void>(null)));
+      expect(result, equals(const Right<dynamic, List<User>>(tResponse)));
 
-      verify(() => repository.createUser(
-            createdAt: params.createdAt,
-            name: params.name,
-            avatar: params.avatar,
-          )).called(1);
+      verify(() => repository.getUsers()).called(1);
 
       verifyNoMoreInteractions(repository);
     },
